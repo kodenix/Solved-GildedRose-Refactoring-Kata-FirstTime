ea91c9337eaddaff52258251fb015f2f62f144d2
Alberto Morales <kodenix@gmail.com>
2023-02-24 23:27:16 +0100

    cambiada negacion por afirmacion

--- app/gilded-rose.ts
+++ app/gilded-rose.ts
@@ -31,7 +31,9 @@ export class GildedRose {
       if (item.quality > 0 && item.name != 'Sulfuras, Hand of Ragnaros') {
         item.quality = item.quality - 1;
       }
-    } else {
+    } 
+    
+    if (item.name === 'Aged Brie' || item.name === 'Backstage passes to a TAFKAL80ETC concert') {
       if (item.quality < 50) {
         item.quality = item.quality + 1;
         if (item.name == 'Backstage passes to a TAFKAL80ETC concert') {
@@ -44,9 +46,11 @@ export class GildedRose {
         }
       }
     }
+    
     if (item.name != 'Sulfuras, Hand of Ragnaros') {
       item.sellIn = item.sellIn - 1;
     }
+
     if (item.sellIn < 0) {
       if (item.name == 'Aged Brie') {
         if (item.quality < 50) {
@@ -55,11 +59,11 @@ export class GildedRose {
       } else {
         if (item.name == 'Backstage passes to a TAFKAL80ETC concert') {
           item.quality = item.quality - item.quality;
-        } else {
-          if (item.quality > 0) {
-            if (item.name != 'Sulfuras, Hand of Ragnaros') {
-              item.quality = item.quality - 1;
-            }
+          return;
+        }
+        if (item.quality > 0) {
+          if (item.name != 'Sulfuras, Hand of Ragnaros') {
+            item.quality = item.quality - 1;
           }
         }
       }
